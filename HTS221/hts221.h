6323d16db348cd4138ce9d96904c3b4af43f9616
PatriciaP666 <pata.palkova@gmail.com>
2023-11-06 21:14:08 +0100

    add init h

--- HTS221/hts221.h
+++ HTS221/hts221.h
@@ -36,5 +36,6 @@
 uint8_t HTS221_read(uint8_t reg_add);
 void HTS221_write(uint8_t data, uint8_t reg_add, uint8_t slave_add);
 void HTS221_array(uint8_t *data, uint8_t reg, uint8_t length);
+uint8_t HTS221_Init();
 
 #endif /* HTS221_H_ */
