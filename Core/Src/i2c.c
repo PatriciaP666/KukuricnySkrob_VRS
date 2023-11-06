07e737a2642e6588e12a575cde4e1c8e3259fa40
PatriciaP666 <pata.palkova@gmail.com>
2023-11-06 17:10:43 +0100

    i2c read

--- Core/Src/i2c.c
+++ Core/Src/i2c.c
@@ -98,3 +98,14 @@ void i2c_write(uint8_t data, uint8_t reg_add, uint8_t slave_add, uint8_t read_fl
 		}
 		LL_I2C_ClearFlag_STOP(I2C1);
 }
+
+uint8_t i2c_read(uint8_t* buffer, uint8_t lenght, uint8_t reg_add, uint8t_t slave_add, uint8_t read_flag)
+{
+	rec_buffer_read = buffer;
+	if(read_flag)
+		{
+			reg_add |= (1 << 7);
+		}
+	end_read_flag = 0;
+	LL_I2C_EnableIT_RX(I2C1);
+}
