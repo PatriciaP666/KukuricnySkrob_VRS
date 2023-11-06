a76d7f73c010cffd3f826dd1222f31df037c1f70
PatriciaP666 <pata.palkova@gmail.com>
2023-11-06 18:36:27 +0100

    lps25hb read

--- LPS25HB/lps25hb.c
+++ LPS25HB/lps25hb.c
@@ -11,7 +11,8 @@ uint8_t lps25hb_add = LPS25HB_DEVICE_ADDRESS0;
 
 uint8t_t LPS25HB_read()
 {
-
+	uint8_t data = 0;
+	return *(i2c_read(&data, 1, reg_add, lps25hb_add, 0));
 }
 
 uint8_t LPS25HB_write()
