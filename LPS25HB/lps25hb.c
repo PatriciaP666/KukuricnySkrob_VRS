8fc57dd9fd76031ce774391123d8917b432377c6
PatriciaP666 <pata.palkova@gmail.com>
2023-11-06 19:40:26 +0100

    lps25hb init

--- LPS25HB/lps25hb.c
+++ LPS25HB/lps25hb.c
@@ -22,5 +22,23 @@ void LPS25HB_write(uint8_t data, uint8_t reg_add, uint8_t slave_add)
 
 uint8_t LPS25HB_Init()
 {
-
+	uint8_t state = 0;
+	uint8_t a = LPS25HB_read(LPS25HB_WHO_AM_I_ADDRESS);
+	if(a == LPS25HB_WHO_AM_I_VALUE)
+	{
+		state = 1;
+	}
+	else
+	{
+		lps25hb_add = LPS25HB_DEVICE_ADDRESS1;
+		a = LPS25HB_read(LPS25HB_WHO_AM_I_ADDRESS);
+		if(a == LPS25HB_WHO_AM_I_VALUE)
+		{
+			state = 1;
+		}
+		else
+		{
+			state = 0;
+		}
+	}
 }
